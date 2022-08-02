21b9c00e6343bab442072d84879087391736f230
Majama007 <105103519+Majama007@users.noreply.github.com>
2022-08-02 04:18:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 document.querySelector("#show-login").addEventListener("click",function(){
-    document.querySelector(".popup ").classList.add("active");
+    document.querySelector(".popup ").classList.add("active"); // id when clicked gives popup form:
+
 });
 document.querySelector(".popup .close-btn").addEventListener("click",function(){
-    document.querySelector(".popup").classList.remove("active");
+    document.querySelector(".popup").classList.remove("active"); // closed botton when clicked ,closes the popup form:
+
 });
 
